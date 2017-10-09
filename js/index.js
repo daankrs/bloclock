e15a1a323fc1dc1122fd770c9a6e5be3d7dadc5c
daankrs <hey@daankorssen.nl>
2017-10-09 14:58:47 +1100

    Updated body

--- js/index.js
+++ js/index.js
@@ -5,3 +5,22 @@ TO-DO:
   - Update function
   -
 */
+var mode = true,
+    block,
+    amountOfBlocks;
+
+    var blocksContainer = document.getElementById('blocksContainer');
+
+function drawBlocks() {
+    if (mode) {
+        amountOfBlocks = 145;
+    } else {
+        amountOfBlocks = 101;
+    }
+    for (var blockNumber = 1; blockNumber < amountOfBlocks; blockNumber++) {
+        block = document.createElement("div");
+        block.id = 'block-' + blockNumber;
+        block.className = 'block';
+        blocksContainer.appendChild(block);
+    }
+}
